ad0de5a767f99cf7a1096d12fd75306bd6b47caa
export60 <2918102736@qq.com>
2026-08-05 21:32:39 +0800

    UPDATE 2026/7/23VerCN1.33

--- README.md
+++ README.md
@@ -6,34 +6,34 @@
 
 ## 数据
   支持水鱼查分器和落雪查分器
-  
+
 ### 水鱼  
   查分器地址：[跳转链接](maimai.diving-fish.com, "水鱼查分器")
-  
+
   需要您亲手点击一次“解锁全曲”并确认，然后点击“导出为CSV”按钮，选择“UTF-8”编码，放至项目根目录下，重命名为df_score.csv。
-  
+
 ### 落雪
   查分器地址：[跳转链接](maimai.lxns.net, "落雪查分器")
-  
+
   左侧栏跳转到“成绩管理”，选择“备份成绩”，选择“导出成绩”，放至项目根目录下，重命名为lx_score.csv。
-  
+
 ## 环境
   Python3 且安装有json、PIL、pandas库，版本没有严格要求。
-  
+
   如若遇到问题，可尝试更换库版本如下：
-  
+
   PIL == 9.3.0
-  
+
   pandas == 2.1.4
 
 ## 使用方法
 
   将导出的存档重命名为 df_score.csv 或 lx_score.csv 后，放在与draw_chart.py文件同一目录下，直接使用Python运行draw_chart.py即可。
-  
+
   您需要输入所生成分表的等级，目前支持 10、10+、11、11+、12、12+、13、13+、14、14+、15、15+。
-  
+
   于draw_chart.py文件开头部分，可以更改Player名称、是否生成高清图，以及选择使用哪种查分器。
-  
+
   程序正常运行后分表图片将输出至./output/目录下。
 
 ## Q&A
@@ -41,8 +41,7 @@
     Q:为什么要做这么一个东西？
     
     A:单纯做着玩的，若有人需要可以随时拿去用。
-
-
+    
     Q:程序报错 UnicodeDecodeError: 'utf-8' codec can't decode byte 0xc5 in position 0: invalid continuation byte. 
-  
+      
     A:使用了类似于Office、WPS等富文本编辑器进行了保存，导致数据格式上发生了一些改变。建议直接重命名导出的csv文件，然后移动至目标目录。
--- draw_chart.py
+++ draw_chart.py
@@ -1,55 +1,40 @@
+import json
 import os.path
 from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageEnhance
 import pandas as pd
 
-# 输入:下面knid列表的内容
-# 输出:带有国服成绩的等积分表
+# 输入:下面levels列表的内容
+# 输出:带有国服成绩的等级分表
+# 数据格式：水鱼: df_score.csv 落雪: lx_score.csv
 # --------------------------------------------------------------
 #    玩家名称
-player = 'CHUNITHM'
+player = ''
 #  是否生成高清图 / 1:生成 2:不生成
 if_generated = 1
+#  分数来源（查分器）/ 1:水鱼 diving-fish 2:落雪 lxns
+source = 2
 
 # --------------------------------------------------------------
-knid = ['10', '10+', '11', '11+', '12', '12+', '13', '13+', '14', '14+', '15']
+levels = ['10', '10+', '11', '11+', '12', '12+', '13', '13+', '14', '14+', '15', '15+']
 difficulty = ['bas', 'adv', 'exp', 'mas', 'ult']
 
 
-def name_replace(name_str):
-    name_str = (
-        name_str.replace('[', '［').replace(']', '］')
-        .replace('#', '＃').replace('>', '＞').replace('_', ' ')
-        .replace('　', ' ')
-    )
-
-    name_str = name_str[0].upper() + name_str[1:]
-    name_str = (name_str.replace('"', '%22').replace('\\', '%5C')
-                .replace('/', '%2F').replace(':', '%3A')
-                .replace('*', '%2a').replace('?', '%3F')
-                .replace('<', '%3C').replace('>', '%3E')
-                .replace('|', '%7C')
-                )
-
-    return name_str
-
-
 def get_songs(srg):
-    with open('idinfo.txt', 'r', encoding='utf-8') as p:
-        info = eval(p.readlines()[0])
-        songs = []
+    with open('index/data.json', 'r', encoding='utf-8') as p:
+        data = json.load(p)
         paint_height = 700 + 150 * 5 + 80 * 4
         a = 10 + srg * 0.5
         b = a + 0.4
+        songs = []
         num = [0, 0, 0, 0, 0]
-        for name, detail in info.items():
-
-            for di in difficulty:
-                if detail[di] != '' and a <= float(detail[di]) <= b:
-                    i = 4 - int((float(detail[di]) * 10 - a * 10))
+        for song in data:
+            for i_ds in range(len(song['ds'])):
+                if  a <= float(song['ds'][i_ds]) <= b:
+                    i = 4 - int((float(song['ds'][i_ds]) * 10 - a * 10))
                     num[i] += 1
                     if num[i] > 11 and (num[i] - 1) % 11 == 0:
                         paint_height += 175
-                    songs.append((detail['id'], name, di, float(detail[di])))
+                    songs.append((i,song['id'],song['title'], difficulty[i_ds], float(song['ds'][i_ds])))
     return songs, num, paint_height
 
 
@@ -57,9 +42,9 @@ def get_songs(srg):
 def level_input():
     while 1:
         srg = -1
-        level = input()
-        for i in range(11):
-            if knid[i] == level:
+        level = input("[10, 10+, 11, 11+, 12, 12+, 13, 13+, 14, 14+, 15, 15+]\n输入你想要查询的等级区间: ")
+        for i in range(12):
+            if levels[i] == level:
                 srg = i
         if srg != -1:
             break
@@ -80,11 +65,17 @@ for i in range(1, 5):
 
 lofk = 'Designed by export60'
 player_text = "Player: "+player
-df = pd.read_csv('save.csv', encoding='utf-8')
-for i in range(10):
-    df.drop(i, inplace=True)
-df = df[(df['定数'] >= a) & (df['定数'] <= b)]
-df.sort_values(by='定数', inplace=True, ascending=False)
+if source == 1:
+    df = pd.read_csv('df_score.csv', encoding='utf-8')
+    df = df[(df['定数'] >= a) & (df['定数'] <= b)]
+    df.sort_values(by='定数', inplace=True, ascending=False)
+
+elif source == 2:
+    df = pd.read_csv('lx_score.csv', encoding='utf-8')
+    df = df[(df['level'] == levels[srg])]
+    df.sort_values(by='score', inplace=True, ascending=False)
+    df = df.drop_duplicates(subset=['id','level_index'])
+
 df.insert(df.shape[1], 'used', 0)
 print(' 成绩读取完成 ')
 
@@ -110,109 +101,213 @@ def draw_chart(img):
             continue
         elif a >= 13 and di == 'adv':
             continue
-        # song (id, name, difficulty, level)
+        # song (i, id, name, difficulty, level)
         for song in songs:
-            if song[2] == di:
-                # df (排行, 乐曲名, 难度, 定数, 分数, Rating, used)
-                for df_row in df.itertuples():
-                    if getattr(df_row, 'used') == 1:
+            if song[3] == di:
+                # 水鱼 df (排名, 乐曲名, 难度, 定数, 分数, Rating, used)
+                if source == 1:
+                    for df_row in df.itertuples():
+                        if getattr(df_row, 'used') == 1:
+                            continue
+
+                            level = float(getattr(df_row, '定数'))
+                            title = getattr(df_row, '乐曲名')
+                            score = getattr(df_row, '分数')
+                        else:
+                            level = song[4]
+                            title = getattr(df_row, 'song_name')
+                            score = getattr(df_row, 'score')
+
+                        if song[2] == title:
+
+                            # 定位细分难度
+                            i = 4 - int((level * 10 - a * 10))
+
+                            # 定位像素点
+                            x = start_x + now[i] * 175
+                            y = start_y[i] + row[i] * 180
+
+                            # 放贴图
+                            if di == 'ult':
+                                color = 'black'
+                            elif di == 'exp':
+                                color = 'red'
+                            elif di == 'adv':
+                                color = 'orange'
+                            elif di == 'bas':
+                                color = 'green'
+                            song_img = Image.open('./img/songs/' + str(song[1]) + '.png').convert('RGBA')
+                            song_img = song_img.resize((150, 150))
+                            if di != 'mas':
+                                x -= 8
+                                y -= 8
+                                song_img = ImageOps.expand(song_img, border=(8, 8, 8, 8), fill=color)
+
+                            # 没打过的歌,调透明度
+                            if int(score) == 0:
+                                fx, fy = song_img.size
+                                for ix in range(fx):
+                                    for iy in range(fy):
+                                        colori = song_img.getpixel((ix, iy))
+                                        colori = colori[:-1] + (180,)
+                                        # song_img.putpixel((ix, iy), colori)
+                                # 降低亮度
+                                song_img = ImageEnhance.Brightness(song_img).enhance(0.5)
+                            img.paste(song_img, (x, y))
+
+                            # 放分数
+                            if int(score) > 0:
+                                l, t, r, b = setFont.getbbox(str(score))
+                                w, h = r - l, b - t
+                                text_x = x + (150 - w) / 2 + 8
+                                text_y = y + 141
+                                if di == 'mas':
+                                    text_y -= 8
+                                    text_x -= 8
+                                draw = ImageDraw.Draw(img)
+                                draw.text((text_x, text_y), str(score), font=setFont, fill='black', stroke_width=4,
+                                          stroke_fill='white')
+
+                            # 放Rank
+                            if int(score) > 0:
+                                rank = None
+                                if int(score) >= 1009000:  # sss+
+                                    rank = Image.open('./img/sssp.png')
+                                elif int(score) >= 1007500:  # sss
+                                    rank = Image.open('./img/sss.png')
+                                elif int(score) >= 1005000:  # ss+
+                                    rank = Image.open('./img/ssp.png')
+                                elif int(score) >= 1000000:  # ss
+                                    rank = Image.open('./img/ss.png')
+                                elif int(score) >= 990000:  # s+
+                                    rank = Image.open('./img/sp.png')
+                                elif int(score) >= 975000:  # s
+                                    rank = Image.open('./img/s.png')
+                                if rank is not None:
+                                    rank = rank.convert('RGBA')
+                                    rank_x, rank_y = x + 12, y - 8
+                                    if di != 'mas':
+                                        rank_x += 8
+                                        rank_y += 8
+                                    img.paste(rank, (rank_x, rank_y))
+
+                            now[i] += 1
+                            if now[i] >= 11:
+                                now[i] = 0
+                                row[i] += 1
+                            df.at[df_row.Index, 'used'] = 1
+                            break
+                else:
+                    # song (i, id, name, difficulty, level)
+                    # 落雪 df (id,song_name,level,level_index,score,rating,
+                    #       over_power,clear,full_combo,full_chain,rank,upload_time,play_time， used)
+                    id = song[1]
+                    if not os.path.isfile('./img/songs/'+str(id)+'.png'):
+                        # 歌被删了
                         continue
-                    name2 = name_replace(getattr(df_row, '乐曲名'))
-                    level = float(getattr(df_row, '定数'))
-                    if song[1] == name2 and a <= level <= a + 0.4:
+                    level = song[4]
+                    title = song[2]
+                    score = 0
+                    for df_row in df.itertuples():
+                        if getattr(df_row, 'song_name') == title and difficulty[getattr(df_row, 'level_index')] == song[3]:
+                            score = getattr(df_row, 'score')
+                            break
+                    # 定位细分难度
+                    i = 4 - int((level * 10 - a * 10))
 
-                        # 定位细分难度
-                        i = 4 - int((level * 10 - a * 10))
+                    # 定位像素点
+                    x = start_x + now[i] * 175
+                    y = start_y[i] + row[i] * 180
 
-                        # 定位像素点
-                        x = start_x + now[i] * 175
-                        y = start_y[i] + row[i] * 180
+                    # 放贴图
+                    if di == 'ult':
+                        color = 'black'
+                    elif di == 'exp':
+                        color = 'red'
+                    elif di == 'adv':
+                        color = 'orange'
+                    elif di == 'bas':
+                        color = 'green'
+                    song_img = Image.open('./img/songs/' + str(song[1]) + '.png').convert('RGBA')
+                    song_img = song_img.resize((150, 150))
+                    if di != 'mas':
+                        x -= 8
+                        y -= 8
+                        song_img = ImageOps.expand(song_img, border=(8, 8, 8, 8), fill=color)
 
-                        # 放贴图
-                        if di == 'ult':
-                            color = 'black'
-                        elif di == 'exp':
-                            color = 'red'
-                        elif di == 'adv':
-                            color = 'orange'
-                        elif di == 'bas':
-                            color = 'green'
-                        song_img = Image.open('./img/' + name2 + '.png').convert('RGBA')
-                        if di != 'mas':
-                            x -= 8
-                            y -= 8
-                            song_img = ImageOps.expand(song_img, border=(8, 8, 8, 8), fill=color)
-                        score = getattr(df_row, '分数')
-                        # 没打过的歌,调透明度
-                        if int(score) == 0:
-                            fx, fy = song_img.size
-                            for ix in range(fx):
-                                for iy in range(fy):
-                                    colori = song_img.getpixel((ix, iy))
-                                    colori = colori[:-1] + (180,)
-                                    # song_img.putpixel((ix, iy), colori)
-                            # 降低亮度
-                            song_img = ImageEnhance.Brightness(song_img).enhance(0.5)
-                        img.paste(song_img, (x, y))
+                    # 没打过的歌,调透明度
+                    if int(score) == 0:
+                        fx, fy = song_img.size
+                        for ix in range(fx):
+                            for iy in range(fy):
+                                colori = song_img.getpixel((ix, iy))
+                                colori = colori[:-1] + (180,)
+                                # song_img.putpixel((ix, iy), colori)
+                        # 降低亮度
+                        song_img = ImageEnhance.Brightness(song_img).enhance(0.5)
+                    img.paste(song_img, (x, y))
 
-                        # 放分数
-                        if int(score) > 0:
-                            l, t, r, b = setFont.getbbox(str(score))
-                            w, h = r - l, b - t
-                            text_x = x + (150 - w) / 2 + 8
-                            text_y = y + 141
-                            if di == 'mas':
-                                text_y -= 8
-                                text_x -= 8
-                            draw = ImageDraw.Draw(img)
-                            draw.text((text_x, text_y), str(score), font=setFont, fill='black', stroke_width=4,
-                                      stroke_fill='white')
+                    # 放分数
+                    if int(score) > 0:
+                        l, t, r, b = setFont.getbbox(str(score))
+                        w, h = r - l, b - t
+                        text_x = x + (150 - w) / 2 + 8
+                        text_y = y + 141
+                        if di == 'mas':
+                            text_y -= 8
+                            text_x -= 8
+                        draw = ImageDraw.Draw(img)
+                        draw.text((text_x, text_y), str(score), font=setFont, fill='black', stroke_width=4,
+                                  stroke_fill='white')
 
-                        # 放Rank
-                        if int(score) > 0:
-                            rank = None
-                            if int(score) >= 1009000:  # sss+
-                                rank = Image.open('./img/sssp.png')
-                            elif int(score) >= 1007500:  # sss
-                                rank = Image.open('./img/sss.png')
-                            elif int(score) >= 1005000:  # ss+
-                                rank = Image.open('./img/ssp.png')
-                            elif int(score) >= 1000000:  # ss
-                                rank = Image.open('./img/ss.png')
-                            elif int(score) >= 990000:  # s+
-                                rank = Image.open('./img/sp.png')
-                            elif int(score) >= 975000:  # s
-                                rank = Image.open('./img/s.png')
-                            if rank is not None:
-                                rank = rank.convert('RGBA')
-                                rank_x, rank_y = x + 12, y - 8
-                                if di != 'mas':
-                                    rank_x += 8
-                                    rank_y += 8
-                                img.paste(rank, (rank_x, rank_y))
+                    # 放Rank
+                    if int(score) > 0:
+                        rank = None
+                        if int(score) >= 1009000:  # sss+
+                            rank = Image.open('./img/sssp.png')
+                        elif int(score) >= 1007500:  # sss
+                            rank = Image.open('./img/sss.png')
+                        elif int(score) >= 1005000:  # ss+
+                            rank = Image.open('./img/ssp.png')
+                        elif int(score) >= 1000000:  # ss
+                            rank = Image.open('./img/ss.png')
+                        elif int(score) >= 990000:  # s+
+                            rank = Image.open('./img/sp.png')
+                        elif int(score) >= 975000:  # s
+                            rank = Image.open('./img/s.png')
+                        if rank is not None:
+                            rank = rank.convert('RGBA')
+                            rank_x, rank_y = x + 12, y - 8
+                            if di != 'mas':
+                                rank_x += 8
+                                rank_y += 8
+                            img.paste(rank, (rank_x, rank_y))
 
-                        now[i] += 1
-                        if now[i] >= 11:
-                            now[i] = 0
-                            row[i] += 1
-                        df.at[df_row.Index, 'used'] = 1
-                        break
+                    now[i] += 1
+                    if now[i] >= 11:
+                        now[i] = 0
+                        row[i] += 1
+                    df.at[df_row.Index, 'used'] = 1
+
 
     return img
 
 
 img = Image.open('./img/bg.png').convert('RGBA').resize((2200, height))
 img = draw_leveltittle(img)
-logo = Image.open('./img/chusan2025.png')
+logo = Image.open('./img/chusan2026.png')
 font_size = 60
 setFont5 = ImageFont.truetype('C:/windows/fonts/SimHei.ttf', 120)
 setFont6 = ImageFont.truetype('C:/windows/fonts/SimHei.ttf', font_size)
-text1 = 'Data is from Diving-fish'
+if source == 1:
+    text1 = 'Data is from Diving-fish'
+else :
+    text1 = 'Data is from lxns.net'
 ImageDraw.Draw(img).text((1400, height - 170), text1, font=setFont6, fill=(195, 33, 54), stroke_fill='white',
                          stroke_width=2)
 ImageDraw.Draw(img).text((1520, height - 100), lofk, font=setFont6, fill=(195, 33, 54), stroke_fill='white',
                          stroke_width=2)
-ImageDraw.Draw(img).text((200, 140), player_text, font=setFont5, fill=(68, 206, 246), stroke_fill='white',
+ImageDraw.Draw(img).text((200, 140), player_text, font=setFont5, fill=(197, 83, 98), stroke_fill='white',
                          stroke_width=6)
 img.paste(logo, (1700, 0), mask=logo)
 
@@ -222,13 +317,13 @@ img = draw_chart(img)
 if os.path.exists('./output') is False:
     os.makedirs('./output')
 if if_generated:
-    img.save('./output/chulevel_chart_' + knid[srg] + '.png')
+    img.save('./output/chulevel_chart_' + levels[srg] + '.png')
 
 compress_rate = 0.38
 
 w, h = img.size
 img = img.resize((int(w * compress_rate), int(h * compress_rate)))
-img.save('./output/chulevel_chart_' + knid[srg] + '_compressed.png')
+img.save('./output/chulevel_chart_' + levels[srg] + '_compressed.png')
 
-print(' 已输出至 output/chulevel_chart_' + knid[srg] + '')
+print(' 已输出至 output/chulevel_chart_' + levels[srg] + '')
 print(' 绘制完成，感谢使用~')
